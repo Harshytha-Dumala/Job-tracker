17e9124d99e8f7e62fdfaf166aa60160af16623b
Harshytha-Dumala <harshytha07@gmail.com>
2026-07-10 17:44:52 +0530

    Add Job entity, repository, controller, MySQL config

---
+++ src/main/java/com/jobtracker/jobtracker/controller/JobController.java
@@ -0,0 +1,41 @@
+package com.jobtracker.jobtracker.controller;
+
+import com.jobtracker.jobtracker.model.Job;
+import com.jobtracker.jobtracker.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobs")
+@CrossOrigin(origins = "*")
+public class JobController {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @GetMapping
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
+
+    @PostMapping
+    public Job addJob(@RequestBody Job job) {
+        return jobRepository.save(job);
+    }
+
+    @PutMapping("/{id}")
+    public Job updateJob(@PathVariable Long id, @RequestBody Job updatedJob) {
+        Job job = jobRepository.findById(id).orElseThrow();
+        job.setCompany(updatedJob.getCompany());
+        job.setRole(updatedJob.getRole());
+        job.setStatus(updatedJob.getStatus());
+        return jobRepository.save(job);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteJob(@PathVariable Long id) {
+        jobRepository.deleteById(id);
+    }
+}
